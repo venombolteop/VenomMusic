9f9c31f7f8da8c6819380a702233ac79517cdc73
Venom <120070832+Itsvenomo@users.noreply.github.com>
2023-09-16 10:01:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Itsvenomo/VenomX",
+    "https://github.com/Itsvenomo/VenomMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
