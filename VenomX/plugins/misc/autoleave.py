32a7d7d421c6e178b2071e3a10d085a09ea2da0c
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2024-02-15 07:26:47 +0530

    Update autoleave.py

--- VenomX/plugins/misc/autoleave.py
+++ VenomX/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001807463997
-                                and i.chat.id != -1001807463997
+                                and i.chat.id != -1001909287098
+                                and i.chat.id != -1001649838443
                             ):
                                 if left == 20:
                                     continue
