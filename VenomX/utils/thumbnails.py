697fcde82eb1774d53d120b0367068f73afb0ef1
Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>
2024-03-05 15:15:59 +0530

    Update thumbnails.py

--- VenomX/utils/thumbnails.py
+++ VenomX/utils/thumbnails.py
@@ -43,71 +43,9 @@ async def get_thumb(videoid):
         if os.path.isfile(f"cache/{videoid}.jpg"):
             return f"cache/{videoid}.jpg"
 
-        url = f"https://www.youtube.com/watch?v={videoid}"
+        url = f"https://lh3.googleusercontent.com/pw/AP1GczPYxt7ufkF6pTEnJUFw4RlN_CTplkoR6rxnEkwT4btBbb2aS4vowH23y4qqA7t2yCT11Ni46zfnE5r7Zz4BqCMTVqphXKKB2oDy-Q_JUw23_BGo1r1ABY_QlA13TafP6Qcg7i7By15qtTZCZ3kRdCE=w720-h1280-s-no-gm?authuser=0}"
         if 1==1:
-            results = VideosSearch(url, limit=1)
-            for result in (await results.next())["result"]:
-                try:
-                    title = result["title"]
-                    title = re.sub("\W+", " ", title)
-                    title = title.title()
-                except:
-                    title = "Unsupported Title"
-                try:
-                    duration = result["duration"]
-                except:
-                    duration = "Unknown Mins"
-                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-                try:
-                    views = result["viewCount"]["short"]
-                except:
-                    views = "Unknown Views"
-                try:
-                    channel = result["channel"]["name"]
-                except:
-                    channel = "Unknown Channel"
-
-            async with aiohttp.ClientSession() as session:
-                async with session.get(f"http://img.youtube.com/vi/{videoid}/maxresdefault.jpg") as resp:
-                    if resp.status == 200:
-                        f = await aiofiles.open(
-                            f"cache/thumb{videoid}.jpg", mode="wb"
-                        )
-                        await f.write(await resp.read())
-                        await f.close()
-
-            youtube = Image.open(f"cache/thumb{videoid}.jpg")
-            image1 = changeImageSize(1280, 720, youtube)
-            image2 = image1.convert("RGBA")
-            background = image2.filter(filter=ImageFilter.BoxBlur(30))
-            enhancer = ImageEnhance.Brightness(background)
-            background = enhancer.enhance(0.6)
-            image2 = background
-                                                                                            
-            circle = Image.open("VenomX/assets/circle.png")
-
-            # changing circle color
-            im = circle
-            im = im.convert('RGBA')
-            color = make_col()
-
-            data = np.array(im)
-            red, green, blue, alpha = data.T
-
-            white_areas = (red == 255) & (blue == 255) & (green == 255)
-            data[..., :-1][white_areas.T] = color
-
-            im2 = Image.fromarray(data)
-            circle = im2
-            # done
-
-            image3 = image1.crop((280,0,1000,720))
-            lum_img = Image.new('L', [720,720] , 0)
-            draw = ImageDraw.Draw(lum_img)
-            draw.pieslice([(0,0), (720,720)], 0, 360, fill = 255, outline = "white")
-            img_arr = np.array(image3)
-            lum_img_arr = np.array(lum_img)
-            final_img_arr = np.dstack((img_arr,lum_img_arr))
+            img_arr,lum_img_arr))
             image3 = Image.fromarray(final_img_arr)
             image3 = image3.resize((600,600))
             
@@ -121,29 +59,4 @@ async def get_thumb(videoid):
             font3 = ImageFont.truetype('VenomX/assets/font2.ttf', 40)
             font4 = ImageFont.truetype('VenomX/assets/font2.ttf', 35)
 
-            image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "NOXIxMUSIC", fill="white", font = font1, align ="middle") 
-            image4.text((670, 150), "NOXIXMUSIC", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
-
-            # title
-            title1 = truncate(title)
-            image4.text((670, 300), text=title1[0], fill="white", stroke_width=1, stroke_fill="white",font = font3, align ="left") 
-            image4.text((670, 350), text=title1[1], fill="white", stroke_width=1, stroke_fill="white", font = font3, align ="left") 
-
-            # description
-            views = f"Views : {views}"
-            duration = f"Duration : {duration} Mins"
-            channel = f"Channel : {channel}"
-
-            image4.text((670, 450), text=views, fill="white", font = font4, align ="left") 
-            image4.text((670, 500), text=duration, fill="white", font = font4, align ="left") 
-            image4.text((670, 550), text=channel, fill="white", font = font4, align ="left")
             
-            image2 = ImageOps.expand(image2,border=20,fill=make_col())
-            image2 = image2.convert('RGB')
-            image2.save(f"cache/{videoid}.jpg")
-            file = f"cache/{videoid}.jpg"
-            return file
-    except Exception as e:
-        print(e)
-        return YOUTUBE_IMG_URL
