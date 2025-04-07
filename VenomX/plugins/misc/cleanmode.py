0377f6d76054a7d59c596a875b04c1550d75f0c0
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2025-04-07 09:51:29 +0530

    Add files via upload

--- VenomX/plugins/misc/cleanmode.py
+++ VenomX/plugins/misc/cleanmode.py
@@ -1,3 +1,7 @@
+
+# All rights reserved.
+#
+
 import asyncio
 from datetime import datetime, timedelta
 
@@ -8,14 +12,21 @@ from pyrogram.raw import types
 
 import config
 from config import adminlist, chatstats, clean, userstats
-from VenomX import app, userbot
-from VenomX.misc import SUDOERS
-from VenomX.utils.database import (get_active_chats,
-                                       get_authuser_names, get_client,
-                                       
- get_served_chats,
- get_served_users, is_cleanmode_on, set_queries)
-                                       
+from strings import command
+from VenomX import app
+from VenomX.utils.database import (
+    get_active_chats,
+    get_authuser_names,
+    get_client,
+    get_particular_top,
+    get_served_chats,
+    get_served_users,
+    get_user_top,
+    is_cleanmode_on,
+    set_queries,
+    update_particular_top,
+    update_user_top,
+)
 from VenomX.utils.decorators.language import language
 from VenomX.utils.formatters import alpha_to_int
 
@@ -33,7 +44,7 @@ async def clean_mode(client, update, users, chats):
     try:
         if not isinstance(update, types.UpdateReadChannelOutbox):
             return
-    except:
+    except Exception:
         return
     if users:
         return
@@ -54,6 +65,152 @@ async def clean_mode(client, update, users, chats):
     await set_queries(1)
 
 
+@app.on_message(command("BROADCAST_COMMAND") & filters.user(config.OWNER_ID))
+@language
+async def braodcast_message(client, message, _):
+    global IS_BROADCASTING
+    if message.reply_to_message:
+        x = message.reply_to_message.id
+        y = message.chat.id
+    else:
+        if len(message.command) < 2:
+            return await message.reply_text(_["broad_5"])
+        query = message.text.split(None, 1)[1]
+
+        if "-nobot" in query:
+            query = query.replace("-nobot", "")
+        if "-pinloud" in query:
+            query = query.replace("-pinloud", "")
+        if "-pin" in query:
+            query = query.replace("-pin", "")
+        if "-assistant" in query:
+            query = query.replace("-assistant", "")
+        if "-user" in query:
+            query = query.replace("-user", "")
+        if query == "":
+            return await message.reply_text(_["broad_6"])
+
+    IS_BROADCASTING = True
+
+    # Bot broadcast inside chats
+    if "-nobot" not in message.text:
+        sent = 0
+        pin = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            if i == config.LOGGER_ID:
+                continue
+            try:
+                m = (
+                    await app.forward_messages(i, y, x)
+                    if message.reply_to_message
+                    else await app.send_message(i, text=query, send_direct=True)
+                )
+                if "-pin" in message.text:
+                    try:
+                        await m.pin(disable_notification=True)
+                        pin += 1
+                    except Exception:
+                        continue
+                elif "-pinloud" in message.text:
+                    try:
+                        await m.pin(disable_notification=False)
+                        pin += 1
+                    except Exception:
+                        continue
+                sent += 1
+            except FloodWait as e:
+                flood_time = int(e.value)
+                if flood_time > 200:
+                    continue
+                await asyncio.sleep(flood_time)
+            except Exception:
+                continue
+        try:
+            await message.reply_text(_["broad_1"].format(sent, pin))
+        except Exception:
+            pass
+
+    # Bot broadcasting to users
+    if "-user" in message.text:
+        susr = 0
+        pin = 0
+        served_users = []
+        susers = await get_served_users()
+        for user in susers:
+            served_users.append(int(user["user_id"]))
+        for i in served_users:
+            try:
+                m = (
+                    await app.forward_messages(i, y, x)
+                    if message.reply_to_message
+                    else await app.send_message(i, text=query, send_direct=True)
+                )
+                if "-pin" in message.text:
+                    try:
+                        await m.pin(both_sides=True, disable_notification=True)
+                        pin += 1
+                    except Exception:
+                        continue
+                elif "-pinloud" in message.text:
+                    try:
+                        await m.pin(both_sides=True, disable_notification=False)
+                        pin += 1
+                    except Exception:
+                        continue
+                susr += 1
+            except FloodWait as e:
+                flood_time = int(e.value)
+                if flood_time > 200:
+                    continue
+                await asyncio.sleep(flood_time)
+            except Exception:
+                pass
+        try:
+            await message.reply_text(_["broad_7"].format(susr, pin))
+        except Exception:
+            pass
+
+    # Bot broadcasting by assistant
+    if "-assistant" in message.text:
+        aw = await message.reply_text(_["broad_2"])
+        text = _["broad_3"]
+        from VenomX.core.userbot import assistants
+
+        for num in assistants:
+            sent = 0
+            client = await get_client(num)
+            contacts = [user.id for user in await client.get_contacts()]
+            async for dialog in client.get_dialogs():
+                if dialog.chat.id == config.LOGGER_ID:
+                    continue
+                if dialog.chat.id in contacts:
+                    continue
+                try:
+                    (
+                        await client.forward_messages(dialog.chat.id, y, x)
+                        if message.reply_to_message
+                        else await client.send_message(dialog.chat.id, text=query)
+                    )
+                    sent += 1
+                except FloodWait as e:
+                    flood_time = int(e.value)
+                    if flood_time > 200:
+                        continue
+                    await asyncio.sleep(flood_time)
+                except Exception as e:
+                    print(e)
+                    continue
+            text += _["broad_4"].format(num, sent)
+        try:
+            await aw.edit_text(text)
+        except Exception:
+            pass
+    IS_BROADCASTING = False
+
 
 async def auto_clean():
     while not await asyncio.sleep(AUTO_SLEEP):
@@ -63,12 +220,32 @@ async def auto_clean():
                     vidid = dic["vidid"]
                     title = dic["title"]
                     chatstats[chat_id].pop(0)
+                    spot = await get_particular_top(chat_id, vidid)
+                    if spot:
+                        spot = spot["spot"]
+                        next_spot = spot + 1
+                        new_spot = {"spot": next_spot, "title": title}
+                        await update_particular_top(chat_id, vidid, new_spot)
+                    else:
+                        next_spot = 1
+                        new_spot = {"spot": next_spot, "title": title}
+                        await update_particular_top(chat_id, vidid, new_spot)
             for user_id in userstats:
                 for dic in userstats[user_id]:
                     vidid = dic["vidid"]
                     title = dic["title"]
                     userstats[user_id].pop(0)
-        except:
+                    spot = await get_user_top(user_id, vidid)
+                    if spot:
+                        spot = spot["spot"]
+                        next_spot = spot + 1
+                        new_spot = {"spot": next_spot, "title": title}
+                        await update_user_top(user_id, vidid, new_spot)
+                    else:
+                        next_spot = 1
+                        new_spot = {"spot": next_spot, "title": title}
+                        await update_user_top(user_id, vidid, new_spot)
+        except Exception:
             continue
         try:
             for chat_id in clean:
@@ -77,36 +254,31 @@ async def auto_clean():
                 for x in clean[chat_id]:
                     if datetime.now() > x["timer_after"]:
                         try:
-                            await app.delete_messages(
-                                chat_id, x["msg_id"]
-                            )
+                            await app.delete_messages(chat_id, x["msg_id"])
                         except FloodWait as e:
                             await asyncio.sleep(e.value)
-                        except:
+                        except Exception:
                             continue
                     else:
                         continue
-        except:
+        except Exception:
             continue
         try:
             served_chats = await get_active_chats()
             for chat_id in served_chats:
                 if chat_id not in adminlist:
                     adminlist[chat_id] = []
-                    admins = (
-                        app.get_chat_members(
-                            chat_id, 
-                            filter=ChatMembersFilter.ADMINISTRATORS
-                        )
-                    ).privileges
+                    admins = app.get_chat_members(
+                        chat_id, filter=ChatMembersFilter.ADMINISTRATORS
+                    )
                     async for user in admins:
-                        if user.can_manage_video_chats:
+                        if user.privileges.can_manage_video_chats:
                             adminlist[chat_id].append(user.user.id)
                     authusers = await get_authuser_names(chat_id)
                     for user in authusers:
                         user_id = await alpha_to_int(user)
                         adminlist[chat_id].append(user_id)
-        except:
+        except Exception:
             continue
 
 
--- VenomX/plugins/misc/seeker.py
+++ VenomX/plugins/misc/seeker.py
@@ -1,7 +1,30 @@
-import asyncio
 
+# All rights reserved.
+#
+import asyncio
+import time
+from datetime import datetime, timedelta
+
+from pyrogram.types import InlineKeyboardMarkup
+
+from strings import get_string
+from VenomX.core.call import Ayush
 from VenomX.misc import db
-from VenomX.utils.database import get_active_chats, is_music_playing
+from VenomX.utils.database import (
+    get_active_chats,
+    get_assistant,
+    get_lang,
+    is_music_playing,
+    set_loop,
+)
+from VenomX.utils.formatters import seconds_to_min
+from VenomX.utils.inline import stream_markup_timer, telegram_markup_timer
+
+from ..admins.callback import wrong
+from .autoleave import autoend
+
+checker = {}
+muted = {}
 
 
 async def timer():
@@ -13,12 +36,156 @@ async def timer():
             playing = db.get(chat_id)
             if not playing:
                 continue
+            file_path = playing[0]["file"]
+            if "index_" in file_path or "live_" in file_path:
+                continue
             duration = int(playing[0]["seconds"])
             if duration == 0:
-                continue
-            if db[chat_id][0]["played"] >= duration:
                 continue
             db[chat_id][0]["played"] += 1
 
 
-asyncio.create_task(timer())
+async def leave_if_muted():
+    while True:
+        await asyncio.sleep(2)
+        for chat_id, details in list(muted.items()):
+            if time.time() - details["timestamp"] >= 60:
+                _ = details["_"]
+                try:
+                    userbot = await get_assistant(chat_id)
+                    members = []
+                    try:
+                        async for member in userbot.get_call_members(
+                            chat_id
+                        ):
+                            if member is None:
+                                continue
+                            members.append(member)
+                    except ValueError:
+                        try:
+                            await Ayush.stop_stream(chat_id)
+                        except Exception:
+                            pass
+                        continue
+
+                    m = next(
+                        (m for m in members if m.chat.id == userbot.id), None
+                    )
+                    if m is None:
+                        continue
+                    is_muted = bool(m.is_muted and not m.can_self_unmute)
+
+                    if is_muted:
+                        await Ayush.stop_stream(chat_id)
+                        await set_loop(chat_id, 0)
+
+                    del muted[chat_id]
+                except Exception:
+                    del muted[chat_id]
+
+
+async def markup_timer():
+    while True:
+        await asyncio.sleep(2)
+        active_chats = await get_active_chats()
+        for chat_id in active_chats:
+            if not await is_music_playing(chat_id):
+                continue
+
+            playing = db.get(chat_id)
+            if not playing:
+                continue
+
+            duration_seconds = int(playing[0]["seconds"])
+
+            try:
+                language = await get_lang(chat_id)
+                _ = get_string(language)
+            except Exception:
+                _ = get_string("en")
+
+            is_muted = False
+            try:
+                userbot = await get_assistant(chat_id)
+                members = []
+                try:
+                    async for member in userbot.get_call_members(chat_id):
+                        if member is None:
+                            continue
+                        members.append(member)
+                except ValueError:
+                    try:
+                        await Ayush.stop_stream(chat_id)
+                    except Exception:
+                        pass
+                    continue
+
+                if not members:
+                    await Ayush.stop_stream(chat_id)
+                    await set_loop(chat_id, 0)
+                    continue
+
+                if len(members) <= 1 and chat_id not in autoend:
+                    autoend[chat_id] = datetime.now() + timedelta(seconds=30)
+
+                m = next((m for m in members if m.chat.id == userbot.id), None)
+                if m is None:
+                    continue
+
+                is_muted = bool(m.is_muted and not m.can_self_unmute)
+                if is_muted:
+
+                    if chat_id not in muted:
+                        muted[chat_id] = {
+                            "timestamp": time.time(),
+                            "_": _,
+                        }
+
+            except Exception:
+                pass
+
+            if duration_seconds == 0:
+                continue
+
+            try:
+                mystic = playing[0]["mystic"]
+                markup = playing[0]["markup"]
+            except Exception:
+                continue
+
+            try:
+                check = wrong[chat_id][mystic.id]
+                if check is False:
+                    continue
+            except Exception:
+                pass
+
+            try:
+                buttons = (
+                    stream_markup_timer(
+                        _,
+                        playing[0]["vidid"],
+                        chat_id,
+                        seconds_to_min(playing[0]["played"]),
+                        playing[0]["dur"],
+                    )
+                    if markup == "stream"
+                    else telegram_markup_timer(
+                        _,
+                        chat_id,
+                        seconds_to_min(playing[0]["played"]),
+                        playing[0]["dur"],
+                    )
+                )
+
+                await mystic.edit_reply_markup(
+                    reply_markup=InlineKeyboardMarkup(buttons)
+                )
+
+            except Exception:
+                continue
+
+
+asyncio.create_task(timer(), name="timer")
+asyncio.create_task(markup_timer(), name="markup_timer")
+asyncio.create_task(leave_if_muted(), name="leave_if_muted")
