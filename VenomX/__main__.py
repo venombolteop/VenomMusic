f730640b55a4c544732db34c4bd46ff5f89de80e
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2025-04-07 09:35:26 +0530

    Add files via upload

--- VenomX/__main__.py
+++ VenomX/__main__.py
@@ -1,29 +1,29 @@
-import asyncio
+
+#
+# All rights reserved.
 import importlib
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from VenomX import LOGGER, app, userbot
+from config import BANNED_USERS
+from VenomX import HELPABLE, LOGGER, app, userbot
 from VenomX.core.call import Ayush
-from VenomX.misc import sudo
 from VenomX.plugins import ALL_MODULES
 from VenomX.utils.database import get_banned_users, get_gbanned
-from config import BANNED_USERS
 
 
 async def init():
-    if (
-        not config.STRING1
-        and not config.STRING2
-        and not config.STRING3
-        and not config.STRING4
-        and not config.STRING5
-    ):
-        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
-        exit()
-    await sudo()
+    if len(config.STRING_SESSIONS) == 0:
+        LOGGER("VenomX").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
+        LOGGER("VenomX").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -31,31 +31,36 @@ async def init():
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
-    except:
+    except Exception:
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("VenomX.plugins" + all_module)
-    LOGGER("VenomX.plugins").info("Successfully Imported Modules...")
+        imported_module = importlib.import_module(all_module)
+
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
+    LOGGER("VenomX.plugins").info("Successfully Imported All Modules ")
     await userbot.start()
     await Ayush.start()
+    LOGGER("VenomX").info("Assistant Started Sucessfully")
     try:
-        await Ayush.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Ayush.stream_call(
+            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+        )
     except NoActiveGroupCall:
         LOGGER("VenomX").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please ensure the voice call in your log group is active."
         )
         exit()
-    except:
-        pass
-    await Ayush.decorators()
-    LOGGER("VenomX").info("ᴠᴇɴᴏᴍxᴍᴜsɪᴄ ʙᴏᴛ sᴛᴀʀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ɴᴏᴡ ᴇɴᴊᴏʏ")
 
+    await Ayush.decorators()
+    LOGGER("VenomX").info("VenomX Started Successfully")
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("VenomX").info("Stopping VenomX Music Bot...")
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(init())
+    app.run(init())
+    LOGGER("VenomX").info("Stopping VenomX! GoodBye")
--- VenomX/logging.py
+++ VenomX/logging.py
@@ -1,19 +1,31 @@
+
+# All rights reserved.
+
+
 import logging
+from logging.handlers import RotatingFileHandler
+
+from config import LOG_FILE_NAME
 
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
-        logging.FileHandler("log.txt"),
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=5000000, backupCount=10),
         logging.StreamHandler(),
     ],
 )
 
-logging.getLogger("httpx").setLevel(logging.ERROR)
-logging.getLogger("pymongo").setLevel(logging.ERROR) 
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("pymongo").setLevel(logging.ERROR)
+logging.getLogger("httpx").setLevel(logging.ERROR)
+
+# Setting ntgcalls logger level and disabling propagation
+ntgcalls_logger = logging.getLogger("ntgcalls")
+ntgcalls_logger.setLevel(logging.CRITICAL)
+ntgcalls_logger.propagate = False
 
 
 def LOGGER(name: str) -> logging.Logger:
