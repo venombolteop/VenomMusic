da3c8153d201bf18dc746856f806cc6f17df51ca
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2025-04-07 09:29:06 +0530

    Add files via upload

--- config/config.py
+++ config/config.py
@@ -1,1 +1,380 @@
 
+# All rights reserved.
+import re
+import sys
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+
+# Get it from my.telegram.org
+
+API_ID = int(getenv("API_ID", ""))
+
+API_HASH = getenv("API_HASH", "")
+
+
+## Get it from @Botfather in Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+
+
+# Database to save your chats and stats... 
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+
+
+CLEANMODE_DELETE_MINS = int(
+    getenv("CLEANMODE_MINS", "5")
+)  # Remember to give value in Seconds
+
+
+# Custom max audio(music) duration for voice chat. set DURATION_LIMIT in variables with your own time(mins), Default to 60 mins.
+
+DURATION_LIMIT_MIN = int(
+    getenv("DURATION_LIMIT", "5400")
+)  # Remember to give value in Minutes
+
+
+
+# Duration Limit for downloading Songs in MP3 or MP4 format from bot
+SONG_DOWNLOAD_DURATION = int(
+    getenv("SONG_DOWNLOAD_DURATION_LIMIT", "5400")
+)  # Remember to give value in Minutes
+
+
+# You'll need a Private Group ID for this.
+LOGGER_ID = int(getenv("LOGGER_ID", ""))
+
+
+# Your User ID.
+OWNER_ID = list(
+    map(int, getenv("OWNER_ID", "7048354045").split())
+)  # Input type must be interger
+
+
+# make your bots privacy from telegra.ph and put your url here
+
+PRIVACY_LINK = getenv(
+    "PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-VenomMusic-01-02"
+)
+
+
+EXTRA_PLUGINS = getenv(
+    "EXTRA_PLUGINS",
+    "False",
+)
+
+# Fill True if you want to load extra plugins
+
+
+EXTRA_PLUGINS_REPO = getenv(
+    "EXTRA_PLUGINS_REPO",
+    "https://github.com/venombolteop/Extra-plugins",
+)
+# Fill here the external plugins repo where plugins that you want to load
+
+
+EXTRA_PLUGINS_FOLDER = getenv("EXTRA_PLUGINS_FOLDER", "plugins")
+
+# Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", False)
+
+# Time after which you're assistant account will leave chats automatically.
+AUTO_LEAVE_ASSISTANT_TIME = int(
+    getenv("ASSISTANT_LEAVE_TIME", 5800)
+)  # Remember to give value in Seconds
+
+
+
+# Get it from http://dashboard.heroku.com/account
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", "")
+
+# You have to Enter the app name which you gave to identify your  Music Bot in Heroku.
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", "")
+
+
+# For customized or modified Repository
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/venombolteop/VenomMusic",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+
+# GIT TOKEN ( if your edited repo is private)
+GIT_TOKEN = getenv(
+    "GIT_TOKEN",
+    "",
+)
+
+
+# Only  Links formats are  accepted for this Var value.
+SUPPORT_CHANNEL = getenv(
+    "SUPPORT_CHANNEL", "https://t.me/Venomowners"
+)
+
+SUPPORT_GROUP = getenv(
+    "SUPPORT_GROUP", "https://t.me/venom_chatz"
+)  
+
+
+
+# Set it true if you want your bot to be private only [You'll need to allow CHAT_ID via /authorize command then only your bot will play music in that chat.]
+PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", "False")
+
+
+# Time sleep duration For Youtube Downloader
+YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "3"))
+
+# Time sleep duration For Telegram Downloader
+TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
+
+
+# Your Github Repo.. Will be shown on /start Command
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/venombolteop/VenomMusic")
+
+
+# Spotify Client.. Get it from https://developer.spotify.com/dashboard
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "19609edb1b9f4ed7be0c8c1342039362")
+SPOTIFY_CLIENT_SECRET = getenv(
+    "SPOTIFY_CLIENT_SECRET", "409e31d3ddd64af08cfcc3b0f064fcbe"
+)
+
+
+# Maximum number of video calls allowed on bot. You can later set it via /set_video_limit on telegram
+VIDEO_STREAM_LIMIT = int(getenv("VIDEO_STREAM_LIMIT", "999"))
+
+
+# Maximum Limit Allowed for users to save playlists on bot's server
+SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "235"))
+
+# MaximuM limit for fetching playlist's track from youtube, spotify, apple links.
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "255"))
+
+
+# Telegram audio  and video file size limit
+
+TG_AUDIO_FILESIZE_LIMIT = int(
+    getenv("TG_AUDIO_FILESIZE_LIMIT", "1073741824")
+)  # Remember to give value in bytes
+
+TG_VIDEO_FILESIZE_LIMIT = int(
+    getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824")
+)  # Remember to give value in bytes
+
+# Checkout https://www.gbmb.org/mb-to-bytes  for converting mb to bytes
+
+
+# If you want your bot to setup the commands automatically in the bot's menu set it to true.
+#
+SET_CMDS = getenv("SET_CMDS", "True")
+
+raw_sessions = getenv("STRING_SESSIONS", "")
+
+# Split the sessions only if raw_sessions is not empty
+STRING_SESSIONS = list(map(str.strip, raw_sessions.split(","))) if raw_sessions else []
+
+
+
+### DONT TOUCH or EDIT codes after this line
+BANNED_USERS = filters.user()
+YTDOWNLOADER = 1
+LOG = 2
+LOG_FILE_NAME = "VenomXlogs.txt"
+TEMP_DB_FOLDER = "tempdb"
+adminlist = {}
+lyrical = {}
+chatstats = {}
+userstats = {}
+clean = {}
+
+autoclean = []
+
+
+# Images
+
+START_IMG_URL = getenv(
+    "START_IMG_URL",
+    "https://te.legra.ph/file/7d9c0fec898bbee09ba95.jpg",
+)
+
+PING_IMG_URL = getenv(
+    "PING_IMG_URL",
+    "https://te.legra.ph/file/95fcd2ec79b527ae3fac4.jpg",
+)
+
+PLAYLIST_IMG_URL = getenv(
+    "PLAYLIST_IMG_URL",
+    "https://te.legra.ph/file/f739e6067725fa88ce8d3.jpg",
+)
+
+GLOBAL_IMG_URL = getenv(
+    "GLOBAL_IMG_URL",
+    "https://te.legra.ph/file/95fcd2ec79b527ae3fac4.jpg",
+)
+
+STATS_IMG_URL = getenv(
+    "STATS_IMG_URL",
+    "https://te.legra.ph/file/c66abbf490158487fdb72.jpg",
+)
+
+TELEGRAM_AUDIO_URL = getenv(
+    "TELEGRAM_AUDIO_URL",
+    "https://te.legra.ph/file/3d130381bf5945c139023.jpg",
+)
+
+TELEGRAM_VIDEO_URL = getenv(
+    "TELEGRAM_VIDEO_URL",
+    "https://te.legra.ph/file/d3663021fb51e14a84aa9.jpg",
+)
+
+STREAM_IMG_URL = getenv(
+    "STREAM_IMG_URL",
+    "https://te.legra.ph/file/248e6858de3f2e37393c1.jpg",
+)
+
+SOUNCLOUD_IMG_URL = getenv(
+    "SOUNCLOUD_IMG_URL",
+    "https://te.legra.ph/file/1b78431fe8de0e497c188.jpg",
+)
+
+YOUTUBE_IMG_URL = getenv(
+    "YOUTUBE_IMG_URL",
+    "https://te.legra.ph/file/98622051acad1988886be.jpg",
+)
+
+SPOTIFY_ARTIST_IMG_URL = getenv(
+    "SPOTIFY_ARTIST_IMG_URL",
+    "https://te.legra.ph/file/c03f25028fa248401d519.jpg",
+)
+
+SPOTIFY_ALBUM_IMG_URL = getenv(
+    "SPOTIFY_ALBUM_IMG_URL",
+    "https://te.legra.ph/file/9fe24bde84b1d31f685a9.jpg",
+)
+
+SPOTIFY_PLAYLIST_IMG_URL = getenv(
+    "SPOTIFY_PLAYLIST_IMG_URL",
+    "https://te.legra.ph/file/7345db59ab5d2c5cb142a.jpg",
+)
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+def seconds_to_time(seconds):
+    minutes = seconds // 60
+    remaining_seconds = seconds % 60
+    return f"{minutes:02d}:{remaining_seconds:02d}"
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+SONG_DOWNLOAD_DURATION_LIMIT = int(time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00"))
+
+if SUPPORT_CHANNEL:
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+        print(
+            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+        )
+        sys.exit()
+
+if SUPPORT_GROUP:
+    if not re.match("(?:http|https)://", SUPPORT_GROUP):
+        print(
+            "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
+        )
+        sys.exit()
+
+if UPSTREAM_REPO:
+    if not re.match("(?:http|https)://", UPSTREAM_REPO):
+        print(
+            "[ERROR] - Your UPSTREAM_REPO url is wrong. Please ensure that it starts with https://"
+        )
+        sys.exit()
+
+if GITHUB_REPO:
+    if not re.match("(?:http|https)://", GITHUB_REPO):
+        print(
+            "[ERROR] - Your GITHUB_REPO url is wrong. Please ensure that it starts with https://"
+        )
+        sys.exit()
+
+
+if PING_IMG_URL:
+    if PING_IMG_URL != "https://te.legra.ph/file/95fcd2ec79b527ae3fac4.jpg":
+        if not re.match("(?:http|https)://", PING_IMG_URL):
+            print(
+                "[ERROR] - Your PING_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+if PLAYLIST_IMG_URL:
+    if PLAYLIST_IMG_URL != "https://te.legra.ph/file/f739e6067725fa88ce8d3.jpg":
+        if not re.match("(?:http|https)://", PLAYLIST_IMG_URL):
+            print(
+                "[ERROR] - Your PLAYLIST_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+if GLOBAL_IMG_URL:
+    if GLOBAL_IMG_URL != "https://te.legra.ph/file/1b78431fe8de0e497c188.jpg":
+        if not re.match("(?:http|https)://", GLOBAL_IMG_URL):
+            print(
+                "[ERROR] - Your GLOBAL_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+
+if STATS_IMG_URL:
+    if STATS_IMG_URL != "https://te.legra.ph/file/c66abbf490158487fdb72.jpg":
+        if not re.match("(?:http|https)://", STATS_IMG_URL):
+            print(
+                "[ERROR] - Your STATS_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+
+if TELEGRAM_AUDIO_URL:
+    if TELEGRAM_AUDIO_URL != "https://te.legra.ph/file/3d130381bf5945c139023.jpg":
+        if not re.match("(?:http|https)://", TELEGRAM_AUDIO_URL):
+            print(
+                "[ERROR] - Your TELEGRAM_AUDIO_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+
+if STREAM_IMG_URL:
+    if STREAM_IMG_URL != "https://te.legra.ph/file/248e6858de3f2e37393c1.jpg":
+        if not re.match("(?:http|https)://", STREAM_IMG_URL):
+            print(
+                "[ERROR] - Your STREAM_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+
+if SOUNCLOUD_IMG_URL:
+    if SOUNCLOUD_IMG_URL != "https://te.legra.ph/file/1b78431fe8de0e497c188.jpg":
+        if not re.match("(?:http|https)://", SOUNCLOUD_IMG_URL):
+            print(
+                "[ERROR] - Your SOUNCLOUD_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+if YOUTUBE_IMG_URL:
+    if YOUTUBE_IMG_URL != "https://te.legra.ph/file/98622051acad1988886be.jpg":
+        if not re.match("(?:http|https)://", YOUTUBE_IMG_URL):
+            print(
+                "[ERROR] - Your YOUTUBE_IMG_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
+
+
+if TELEGRAM_VIDEO_URL:
+    if TELEGRAM_VIDEO_URL != "https://te.legra.ph/file/d3663021fb51e14a84aa9.jpg":
+        if not re.match("(?:http|https)://", TELEGRAM_VIDEO_URL):
+            print(
+                "[ERROR] - Your TELEGRAM_VIDEO_URL url is wrong. Please ensure that it starts with https://"
+            )
+            sys.exit()
