712a2b3cf144aac464a5fae4231e18cda60dc7c3
venom bolteop <145913044+venombolteop@users.noreply.github.com>
2025-04-07 09:42:38 +0530

    Add files via upload

--- VenomX/utils/inline/help.py
+++ VenomX/utils/inline/help.py
@@ -1,96 +1,21 @@
-from typing import Union
 
+# All rights reserved.
+#
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+from config import SUPPORT_GROUP
 from VenomX import app
 
 
-def help_pannel(_, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
-    second = [
-        InlineKeyboardButton(
-            text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
-        ),
-    ]
-    mark = second if START else first
+def support_group_markup(_):
     upl = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
-                    text=_["H_B_1"],
-                    callback_data="help_callback hb1",
+                    text=_["S_B_3"],
+                    url=SUPPORT_GROUP,
                 ),
-                InlineKeyboardButton(
-                    text=_["H_B_2"],
-                    callback_data="help_callback hb2",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_3"],
-                    callback_data="help_callback hb3",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb6",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_8"],
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"],
-                    callback_data="help_callback hb9",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_10"],
-                    callback_data="help_callback hb10",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_11"],
-                    callback_data="help_callback hb11",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_12"],
-                    callback_data="help_callback hb12",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_13"],
-                    callback_data="help_callback hb13",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_14"],
-                    callback_data="help_callback hb14",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_15"],
-                    callback_data="help_callback hb15",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_16"],
-                    callback_data="help_callback hb16",
-                ),
-            ],
-            mark,
+            ]
         ]
     )
     return upl
@@ -101,9 +26,9 @@ def help_back_markup(_):
         [
             [
                 InlineKeyboardButton(
-                    text=_["BACK_BUTTON"],
-                    callback_data=f"settings_back_helper",
+                    text=_["BACK_BUTTON"], callback_data=f"settings_back_helper"
                 ),
+                InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close"),
             ]
         ]
     )
@@ -114,9 +39,8 @@ def private_help_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_4"],
-                url=f"https://t.me/{app.username}?start=help",
-            ),
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?start=help"
+            )
         ],
     ]
     return buttons
